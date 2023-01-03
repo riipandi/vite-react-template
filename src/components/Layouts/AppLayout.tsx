26c980f40cae08bb631c77e51612f2dc1dc21e05
Aris Ripandi <riipandi@gmail.com>
2023-01-03 22:59:31 +0700

    add navbar

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
+import { NavBarCollapseLight } from '../Navbar'
+
 export function AppLayout() {
   return (
-    <main className='flex h-full min-h-screen items-center bg-gray-100 py-16 dark:bg-slate-900'>
+    <main className='flex h-full min-h-screen items-center bg-gray-100 dark:bg-slate-900'>
+      <NavBarCollapseLight />
       <Outlet />
     </main>
   )
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,4 @@
+export * from './NavBarExpandLight'
+export * from './NavBarExpandDark'
+export * from './NavBarCollapseDark'
+export * from './NavBarCollapseLight'

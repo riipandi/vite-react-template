3483c3395a5a17b72fc35f3a3438d34b9ddeb6f6
Aris Ripandi <riipandi@gmail.com>
2023-06-01 10:33:46 +0700

    fix typo sidebar title

--- src/components/Navbar/NavBarExpand.tsx
+++ src/components/Navbar/NavBarExpand.tsx
@@ -10,7 +10,7 @@ export function NavBarExpand() {
         >
           <path d='M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z' />
         </svg>
-        <span className='ml-2 text-sm font-bold'>Icon+ DNS Manager</span>
+        <span className='ml-2 text-sm font-bold'>ReactiVite</span>
       </a>
       <div className='w-full px-2'>
         <div className='mt-3 flex w-full flex-col items-center border-t border-gray-200 dark:border-gray-700'>

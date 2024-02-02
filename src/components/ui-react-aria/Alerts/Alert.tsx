c652e57ae457220ba0d188c7075d17cdb6f32455
Aris Ripandi <riipandi@gmail.com>
2024-02-03 05:21:20 +0700

    fix: color contrast ratio

--- src/components/ui-react-aria/Alerts/Alert.tsx
+++ src/components/ui-react-aria/Alerts/Alert.tsx
@@ -15,8 +15,8 @@ export const Alert: FC<AlertProps> = ({ children, variant = 'info', className })
         variant == 'info' && 'border-blue-200 bg-primary-50 text-blue-600',
         variant == 'success' && 'border border-green-200 bg-green-50 text-green-600',
         variant == 'destructive' &&
-          'border border-destructive-200 bg-destructive-50 text-sm text-destructive-600',
-        variant == 'warning' && 'border border-orange-200 bg-orange-50 text-sm text-orange-600',
+          'border-destructive-200 bg-destructive-50 text-destructive-600 border text-sm',
+        variant == 'warning' && 'border border-orange-200 bg-orange-50 text-sm text-orange-800',
         variant == 'subtle' && 'border-white/[.1 bg-white/[.1] text-gray-600 dark:text-gray-400',
         className
       )}

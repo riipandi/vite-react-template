aa43bbd26ea155ae015a6dd8c94f7491a98b22fe
Aris Ripandi <riipandi@gmail.com>
2026-09-02 00:25:18 +0700

    chore: add component docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 import stylex from '@stylexjs/unplugin/vite'
 import { resolve } from 'node:path'
+import remarkGfm from 'remark-gfm'
 import { mergeConfig } from 'vite'
 
 const config: StorybookConfig = {
   stories: ['./stories/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
-    '@storybook/addon-docs',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm]
+          }
+        }
+      }
+    },
     '@storybook/addon-links',
     '@storybook/addon-mcp',
     '@storybook/addon-vitest',
